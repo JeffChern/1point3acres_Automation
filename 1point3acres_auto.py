7ba16490eaa1351c9bb4dd27bec92742c34a97a7
Jeff Chen <jeff@Jeffs-MacBook-Pro.local>
2017-06-23 16:45:17 +0800

    edited chrome driver path
    Added README & Edited code

--- 1point3acres_auto.py
+++ 1point3acres_auto.py
@@ -4,13 +4,14 @@
 from selenium import webdriver
 import time
 
-chrome_path = '/Users/jeff/Developing/chromedriver'
+chrome_path = '__YOUR__CHROME_DRIVER_PATH__'
 browser = webdriver.Chrome(chrome_path)
 browser.get('http://www.1point3acres.com/bbs/')
 browser.find_element_by_id("ls_username").send_keys("__USERNAME__")
 browser.find_element_by_id("ls_password").send_keys("__PASSWORD__")
 time.sleep(5)
 browser.find_element_by_css_selector("button.pn.vm").click()
+time.sleep(3)
 browser.find_element_by_xpath("//div[@id='um']/p/a[4]/font").click()
 time.sleep(3)
 browser.find_element_by_css_selector("#fd > center > img").click()
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
+
+
 # 1point3acres_AutoLogin
+### Installation
+
+1. Install [Selenium](https://pypi.python.org/pypi/selenium) 
+
+```
+$ pip install -U selenium
+```
+2. Dowload [Chrome driver](https://sites.google.com/a/chromium.org/chromedriver/)
+
+3. Set crontab 
+
+```
+$ crontab -e 
+$ @daily python YOUR__PROGRAM__PATH  
+```
